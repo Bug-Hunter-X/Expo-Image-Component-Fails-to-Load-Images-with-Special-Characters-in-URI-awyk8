5305baf01cb52fe4ef73f69c240609248326969f
BugHunter-X <askzenai@gmail.com>
2025-01-17 13:56:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using Expo's `Image` component with a URI that contains special characters, such as spaces or accented characters. The image fails to load, and no error is thrown in the console. This makes debugging difficult, as there's no clear indication of what's wrong.
